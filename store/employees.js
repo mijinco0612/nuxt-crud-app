c6ec7579e0b581d3f97e9ded9e6c1daca5216457
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-04 16:57:59 +0900

    storeを定義

---
+++ store/employees.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+  employees:[{id:1,name:'defaultName1',role:'defaultRole'},{id:2,name:'defaultName2',role:'defaultRole'},]
+})
+
+export const mutations = {
+  updateEmployees : function(state, payload) {
+    state.employees = payload
+  }
+}
+
+export const actions = {
+  fetchEmployeesAction (context) {
+
+    const testPayload = {id:99,name:'testPayloadName',role:'testPayloadRole'}
+
+    context.commit('updateEmployees',testPayload)
+  }
+}
