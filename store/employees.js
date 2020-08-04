ada77248f041e49068b228af72edc951937d73d8
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-04 17:59:59 +0900

    actions を修正 非同期でAPI通信できるようにした。

--- store/employees.js
+++ store/employees.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 export const state = () => ({
   employees:[{id:1,name:'defaultName1',role:'defaultRole'},{id:2,name:'defaultName2',role:'defaultRole'},]
 })
@@ -9,10 +10,19 @@ export const mutations = {
 }
 
 export const actions = {
-  fetchEmployeesAction (context) {
-
+ /* fetchEmployeesAction (context) {
     const testPayload = {id:99,name:'testPayloadName',role:'testPayloadRole'}
-
     context.commit('updateEmployees',testPayload)
+  }
+*/
+  fetchEmployeesAction : async function (context) {
+
+    axios.get('/api/employees').then((response) => {
+      context.commit('updateEmployees', response.data)
+      return true;
+    }, (err) => {
+      console.log(err)
+      return false;
+    })
   }
 }
