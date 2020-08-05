ad34cb6348405a3faf730e2420ee0f2853a7228c
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-05 16:20:17 +0900

    element-ui のcomponentを使用して画面を再構築

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,72 @@
 <template>
   <div class="container">
     <div>
-      <p>{{ $store.state.employees.employees}}</p>
-      <p>{{ createEmployee }}</p>
-      <label>name</label>
-      <input type="text" v-model="createEmployee.name">
-      <label>role</label>
-      <input type="text" v-model="createEmployee.role">
-      <button v-on:click="$store.dispatch('employees/createEmployeesAction',createEmployee)">create</button>
 
-      <ul v-for="employee in employees">
-        <li>
-          <input type="text" v-model="employee.name">
-          <input type="text" v-model="employee.role">
-          <button v-on:click="$store.dispatch('employees/updateEmployeesAction',employee)">update</button>
-          <button v-on:click="$store.dispatch('employees/deleteEmployeesAction',employee.id)">delete</button>
-        </li>
-      </ul>
-      <button v-on:click="$store.dispatch('employees/fetchEmployeesAction')">fetch</button>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>Create New Employee</span>
+          <el-button  style="float: right;" type="primary" v-on:click="$store.dispatch('employees/createEmployeesAction',createEmployee)">create</el-button>
+        </div>
+        <div>
+          <el-form  class="demo-form-inline">
+            <el-form-item label="employee name">
+              <el-input type="text" v-model="createEmployee.name"></el-input>
+            </el-form-item>
+            <el-form-item label="employee role">
+              <el-input type="text" v-model="createEmployee.role"></el-input>
+            </el-form-item>
+            <el-form-item >
+
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-card>
+
+
+      <el-table
+        :data="employees"
+        style="width: 80%;margin-right: auto;margin-left: auto">
+
+        <el-table-column
+          label="Name"
+          width="300">
+          <template slot-scope="scope">
+
+              <div slot="reference" class="name-wrapper">
+                <el-input type="text" v-model="scope.row.name"></el-input>
+              </div>
+
+          </template>
+        </el-table-column>
+
+        <el-table-column
+          label="Role"
+          width="300">
+          <template slot-scope="scope">
+            <el-input type="text" v-model="scope.row.role"></el-input>
+          </template>
+        </el-table-column>
+
+
+        <el-table-column
+          label="Operations">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              v-on:click="update(scope.row)">
+              Edit
+            </el-button>
+
+            <el-button
+              size="mini"
+              type="danger"
+              v-on:click="$store.dispatch('employees/deleteEmployeesAction',scope.row.id)">
+              Delete
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
     </div>
   </div>
 </template>
@@ -26,8 +75,7 @@
 export default {
   data(){
     return{
-      createEmployee : {name : '' ,role:'' },
-      updateEmployee : {id:'' , name : '' ,role:'' },
+      createEmployee : {name : '' ,role:'' }
     }
   },
   mounted() {
@@ -37,10 +85,45 @@ export default {
       employees(){
         return  this.$store.state.employees.employees;
       }
+    },
+  methods: {
+    update(employee) {
+      this.$notify({
+        title: 'Update',
+        message: '更新しました',
+        type: 'success',
+        offset: 100,
+        duration:1500
+      })
+      this.$store.dispatch('employees/updateEmployeesAction',employee)
     }
+  }
 }
 </script>
 
 <style>
+  .text {
+    font-size: 14px;
+  }
 
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50px;
+  }
 </style>
