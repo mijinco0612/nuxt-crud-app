fd6b3a28924397acb72a7ddf6e044006430d9900
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-05 13:04:39 +0900

    update用バインディングを追加した。

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <p>{{ $store.state.employees.employees }}</p>
+      <p>{{ $store.state.employees.employees}}</p>
       <p>{{ createEmployee }}</p>
       <label>name</label>
       <input type="text" v-model="createEmployee.name">
@@ -11,7 +11,9 @@
 
       <ul v-for="employee in employees">
         <li>
-          {{employee}}
+          <input type="text" v-model="employee.name">
+          <input type="text" v-model="employee.role">
+          <button v-on:click="$store.dispatch('employees/updateEmployeesAction',employee)">update</button>
           <button v-on:click="$store.dispatch('employees/deleteEmployeesAction',employee.id)">delete</button>
         </li>
       </ul>
@@ -24,17 +26,18 @@
 export default {
   data(){
     return{
-      createEmployee : {name : '' ,role:'' }
+      createEmployee : {name : '' ,role:'' },
+      updateEmployee : {id:'' , name : '' ,role:'' },
     }
   },
   mounted() {
     this.$store.dispatch('employees/fetchEmployeesAction')
   },
   computed: {
-    employees() {
-      return this.$store.state.employees.employees;
+      employees(){
+        return  this.$store.state.employees.employees;
+      }
     }
-  }
 }
 </script>
 
